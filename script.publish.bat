695289003e5d96586b338536d396da8f579bde8a
Tito Bouzout <tito.bouzout@gmail.com>
2023-11-01 14:49:11 -0300

    Add favicon

--- script.publish.bat
+++ script.publish.bat
@@ -14,6 +14,7 @@ call cpx "./src/public/quack.png" "./dist/public/"
 call cpx "./src/public/video.mp4" "./dist/public/"
 call cpx "./src/public/flair.css" "./dist/public/"
 call cpx "./src/public/logo.svg" "./dist/public/"
+call cpx "./src/public/favicon.ico" "./dist/public/"
 
 :: delete published docs
 call rmdir "S:/www/quack.uy/client/pota.quack.uy/" /s /q
