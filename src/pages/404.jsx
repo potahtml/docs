a6b889deaacbe43a317ce394d5178a556350258f
Tito Bouzout <tito.bouzout@gmail.com>
2024-01-05 17:38:23 -0300

    add noindex for 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,6 +8,10 @@ export default function FourZeroFour() {
 			<Header title="console.log('404 Not Found')">
 				<Head>
 					<title>404 Not Found - pota</title>
+					<meta
+						name="robots"
+						content="noindex"
+					/>
 				</Head>
 				throw new Error ("this should never happen")
 			</Header>
