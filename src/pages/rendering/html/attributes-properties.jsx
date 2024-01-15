c7f513f6ef7e705d28dda184950629bf49bce740
Tito Bouzout <tito.bouzout@gmail.com>
2024-01-15 10:16:07 -0300

    update docs

--- src/pages/rendering/html/attributes-properties.jsx
+++ src/pages/rendering/html/attributes-properties.jsx
@@ -1,4 +1,4 @@
-import { render, signal, memo, html, ref, effect } from 'pota'
+import { render, html, ref, effect } from 'pota'
 
 function Component() {
   const element = ref()
@@ -8,12 +8,20 @@ function Component() {
 
   return html`
     <test-test
-      attr:example-attr="${1}"
-      prop:example-prop="${2}"
-      boolean="${true}"
-      dash-boolean="${true}"
+      attr:example-attr-1="${1}"
+      attr:example-attr-true="${true}"
+      attr:example-attr-false="${false}"
+      prop:example-prop-2="${2}"
+      prop:example-prop-2-true="${true}"
+      prop:example-prop-2-false="${false}"
+      booleantrue="${true}"
+      booleanfalse="${false}"
+      dash-boolean-true="${true}"
+      dash-boolean-false="${false}"
       string="${'hola'}"
       ref="${element}"
+      bool:booltruetest="${true}"
+      bool:boolfalsetest="${false}"
     >
       attributes/props test
     </test-test>
