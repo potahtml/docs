b18360435b147453b7dda39c1005b72e6b91adb9
Tito Bouzout <tito.bouzout@gmail.com>
2024-01-17 01:19:34 -0300

    update docs

--- release.bat
+++ release.bat
@@ -6,6 +6,7 @@ call git commit -m "update docs"
 call git push
 
 call cpx ./src/** S:/www/quack.uy/client/pota.quack.uy/
+call cpx ./node_modules/** S:/www/quack.uy/client/pota.quack.uy/node_modules/
 
 :: save changes on pota.quack.uy
 
