26c5c2d01b870e207f02da5f5e2fa9c9d9184d8e
Tito Bouzout <tito.bouzout@gmail.com>
2024-01-17 00:53:04 -0300

    update docs

--- src/routes.jsx
+++ src/routes.jsx
@@ -56,7 +56,7 @@ export default function Routes() {
 				/>
 				<Route
 					path="HTML$"
-					children={lazy(() => import('./pages/rendering/html.jsx'))}
+					children={lazy(() => import('./pages/rendering/HTML.jsx'))}
 				/>
 
 				<Route path="children/">
