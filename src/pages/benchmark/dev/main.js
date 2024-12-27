d1453bdd0c05e38b113fe88bd56af65cc99bec88
Tito Bouzout <tito.bouzout@gmail.com>
2024-12-27 13:22:06 -0300

    update docs

--- src/pages/benchmark/dev/main.js
+++ src/pages/benchmark/dev/main.js
@@ -485,14 +485,13 @@
 
 	// SIGNAL
 
-	/**
-	 * @template const T
-	 * @type SignalObject<T>
-	 * @returns {SignalObject<T>}
-	 */
+	/** @template in T */
 	class Signal {
+	  /** @private */
 	  value;
+	  /** @private */
 	  observers;
+	  /** @private */
 	  observerSlots;
 
 	  // options:
@@ -500,18 +499,22 @@
 	  // save
 
 	  // `prev` if option save was given
-
+	  /**
+	   * @param {T} [value]
+	   * @param {SignalOptions} [options]
+	   */
 	  constructor(value, options) {
 	    this.value = value;
 	    if (options) {
 	      assign(this, options);
 	      if (this.save) {
+	        /** @private */
 	        this.prev = value;
 	      }
 	    }
 	    this.read = markReactive(this.read);
 	  }
-	  /** @type SignalAccessor<T> */
+	  /** @returns SignalAccessor<T> */
 	  read = () => {
 	    // checkReadForbidden()
 
@@ -534,7 +537,10 @@
 	    }
 	    return this.value;
 	  };
-	  /** @type SignalSetter<T> */
+	  /**
+	   * @param {T} [value]
+	   * @returns SignalSetter<T>
+	   */
 	  write = value => {
 	    if (this.equals === false || !this.equals(this.value, value)) {
 	      if (this.save) {
@@ -563,19 +569,26 @@
 	    }
 	    return false;
 	  };
-	  /** @type SignalUpdate<T> */
+	  /**
+	   * @param {T} [value]
+	   * @returns SignalUpdate<T>
+	   */
 	  update = value => {
 	    if (isFunction(value)) {
 	      value = value(this.value);
 	    }
 	    return this.write(value);
 	  };
+	  /** @private */
 	  equals(a, b) {
 	    return a === b;
 	  }
 	  *[Symbol.iterator]() {
+	    /** @type SignalAccessor<T> */
 	    yield this.read;
+	    /** @type SignalSetter<T> */
 	    yield this.write;
+	    /** @type SignalUpdate<T> */
 	    yield this.update;
 	  }
 	}
@@ -606,10 +619,9 @@
 	/**
 	 * Creates a signal
 	 *
-	 * @template const T
+	 * @template T
 	 * @param {T} [initialValue] - Initial value of the signal
 	 * @param {SignalOptions} [options] - Signal options
-	 * @returns {SignalObject<T>}
 	 */
 	/* #__NO_SIDE_EFFECTS__ */
 	function signal(initialValue, options = undefined) {
