4318e5e4ac83bbbe060175ee6b47a5497859363d
Tito Bouzout <tito.bouzout@gmail.com>
2025-07-16 15:52:56 -0300

    update docs

--- src/lib/transform.js
+++ src/lib/transform.js
@@ -6,7 +6,9 @@ export async function transform(code) {
 		import(
 			'https://esm.sh/@babel/preset-typescript@7.27.1?bundle=all&keep-names'
 		),
-		import('https://esm.sh/pota/babel-preset?bundle=all&keep-names'),
+		import(
+			'https://esm.sh/pota/babel-preset?bundle=all&keep-names&deps=@babel/core@7.25.9'
+		),
 	])
 		.then(imports => {
 			const [babel, typescript, pota] = imports
