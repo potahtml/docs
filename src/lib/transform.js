7be85064ca0d0c3f775c96010568b1031ebbaed0
Tito Bouzout <tito.bouzout@gmail.com>
2025-07-16 17:38:48 -0300

    update docs

--- src/lib/transform.js
+++ src/lib/transform.js
@@ -2,12 +2,23 @@ import { prettier } from './prettier.js'
 
 export async function transform(code) {
 	return Promise.all([
-		import('https://esm.sh/@babel/core?bundle=all'),
-		import('https://esm.sh/@babel/preset-typescript?bundle=all'),
 		import('https://esm.sh/pota/babel-preset?bundle=all'),
+		import(
+			'https://esm.sh/@babel/preset-typescript?target=es2022&deps=@babel/helper-globals'
+		),
+		import('https://esm.sh/@babel/core?bundle=all'),
+		//	,
 	])
 		.then(imports => {
-			const [babel, typescript, pota] = imports
+			const [
+				//
+				pota,
+				//
+				typescript,
+				//
+				babel,
+				//
+			] = imports
 
 			return prettier(
 				babel.transform(code, {
