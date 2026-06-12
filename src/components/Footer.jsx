0128c302521125c8eb2ece6070f7501dafa1ec0c
Tito Bouzout <tito.bouzout@gmail.com>
2026-06-12 10:10:09 -0300

    update docs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
+import { location } from 'pota/use/location'
 import styles from './Footer.module.css'
 
 export function Footer() {
+	const editHref = () =>
+		`https://github.com/potahtml/pota/edit/master/documentation/content${location.pathname()}.md`
+
 	return (
 		<footer class={styles.footer}>
 			<div>
@@ -9,14 +13,22 @@ export function Footer() {
 			</div>
 			<span>
 				<a href="/thanks">thanks</a> ·{' '}
-				<a
-					href="https://github.com/potahtml/pota/tree/master/documentation/content"
-					target="_blank"
-					rel="noopener"
-				>
-					edit
-				</a>{' '}
-				· made with{' '}
+				{()=>location.pathname() !== '/' &&
+					location.pathname() !== '/playground' &&
+					location.pathname() !== '/cheatsheet' &&
+					location.pathname() !== '/thanks' && (
+						<>
+							<a
+								href={editHref()}
+								target="_blank"
+								rel="noopener"
+							>
+								edit
+							</a>{' '}
+							·{' '}
+						</>
+					)}{' '}
+				made with{' '}
 				<a
 					href="https://github.com/potahtml/pota"
 					target="_blank"
